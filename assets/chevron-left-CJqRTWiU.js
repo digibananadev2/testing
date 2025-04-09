2adaa6cddcdbb2b4a11fd53b4e8a6557005397b6
developer2 <digibanana.dev2@gmail.com>
2025-04-09 10:32:21 +0530

    testing

---
+++ assets/chevron-left-CJqRTWiU.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-RWvjyKq-.js";const o=t=>e.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:"1em",height:"1em",viewBox:"0 0 20 20",fill:"none",...t},e.createElement("path",{d:"M12.7083 5L7.5 10.2083L12.7083 15.4167",stroke:"currentColor",strokeWidth:1.5,strokeLinecap:"round",strokeLinejoin:"round"}));export{o as S};
